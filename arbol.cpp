92ac01b74c2b3448374c5a2dc411d4a34d0fc26c
GD <geny.d.mtz@hotmail.com>
2020-06-05 19:25:38 -0500

    Programación de la función para insertar nodos

--- arbol.cpp
+++ arbol.cpp
@@ -26,7 +26,9 @@ Nodo* Arbol::getRaiz(){
 }
 
 /*	Revisa si la raíz del árbol existe. Si no, crea un nuevo nodo como la raíz.
-	Si ya existe, se el nodo se inserta en su lugar correspondiente. */
+	Si ya existe, se el nodo se inserta en su lugar correspondiente. 
+	
+*/
 Nodo* Arbol::insertarNodo(int dato){
 	bool doble = false; // toma el valor true cuando se intenta ingresar un dato que ya está en el árbol
 	Nodo *nuevo = new Nodo(dato);
@@ -35,13 +37,34 @@ Nodo* Arbol::insertarNodo(int dato){
 		cout<<"\n\tEl nodo se inserto como raiz del arbol..."<<endl;
 		raiz = nuevo;
 		altura = 1;
-		//cout<<"raiz: "<<raiz->getInfo()<<endl;
 	}else{
 		nodo = raiz;
 		while(!doble&&nuevo->getPadre()==NULL){
 			if(dato==nodo->getInfo()){
 				cout<<"\n\tEl nodo ya se encuentra en el arbol..."<<endl;
 				doble = true;
+			}else if(dato < nodo->getInfo()){
+				if(nodo->getIzq()==NULL){
+					nuevo->setPadre(nodo);
+					nodo->setIzq(nuevo);
+					nuevo->setTipo(1);
+					cout<<"\n\tDato insertado correctamente..."<<endl;
+					cout<<"\tNodo padre: "<<nuevo->getPadre()->getInfo()<<endl;
+					cout<<"\tHijo: "<<nuevo->getTipo()<<endl;
+				}else{
+					nodo = nodo->getIzq();
+				}
+			}else{ //if(info>nodo->dato)
+				if(nodo->getDer()==NULL){
+					nuevo->setPadre(nodo);
+					nodo->setDer(nuevo);
+					nuevo->setTipo(2);
+					cout<<"\n\tDato insertado correctamente..."<<endl;
+					cout<<"\tNodo padre: "<<nuevo->getPadre()->getInfo()<<endl;
+					cout<<"\tHijo: "<<nuevo->getTipo()<<endl;
+				}else{
+					nodo = nodo->getDer();
+				}				
 			}
 		}
 	}
